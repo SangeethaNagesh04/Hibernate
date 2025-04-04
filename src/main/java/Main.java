b370407bf273e0a2a140ef209cab7fd5a0b45159
sangeetha <sangeethanageshgit1@gmail.com>
2025-04-04 23:33:35 +0530

    HQL query

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,7 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.query.Query;
 
 import java.rmi.StubNotFoundException;
 import java.util.List;
@@ -9,46 +10,23 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
 
-        Alien a1 = new Alien();
-        a1.setAid(101);
-        a1.setAname("sangeetha");
-        a1.setTech("java");
-
-        Laptop l1 = new Laptop();
-        l1.setLid(1);
-        l1.setBrand("Asus");
-        l1.setRam(16);
-
-        Laptop l2 = new Laptop();
-        l2.setLid(2);
-        l2.setBrand("Hp");
-        l2.setRam(32);
-
-        a1.setLaptop(List.of(l1,l2));
-
         Configuration cfg = new Configuration();
-        cfg.addAnnotatedClass(Alien.class);
         cfg.addAnnotatedClass(Laptop.class);
         cfg.configure();
 
         SessionFactory sf = cfg.buildSessionFactory();
         Session session = sf.openSession();
 
+        Query query = session.createQuery("from Laptop where brand='Hp'");
 
-        Transaction transaction = session.beginTransaction();
+        List<Laptop> laptops = query.getResultList();
 
-        session.persist(l1);
-        session.persist(l2);
-        session.persist(a1);
-
-        transaction.commit();
-
-
+        for (Laptop laptop : laptops) {
+            System.out.println(laptop);
+        }
 
         session.close();
-        Session session1 = sf.openSession();
-        Alien a2 = session1.get(Alien.class, 101);
-        // System.out.println(a2);
+
         sf.close();
 
 
